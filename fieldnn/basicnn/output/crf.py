3bd97bd7a4e5a2b7dc78c74e8b5d783b70da25cc
Junjie Luo <jjluo@umd.edu>
2023-04-19 17:14:57 -0400

    for output block

--- fieldnn/basicnn/output/crf.py
+++ fieldnn/basicnn/output/crf.py
@@ -1,9 +1,6 @@
-
-from .helper import pad_packed_sequence
-import numpy as np
-
 from typing import List, Optional
-
+# from .helper import pad_packed_sequence
+import numpy as np
 import torch
 import torch.nn as nn
 
